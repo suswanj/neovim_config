d2a5ce23d213dc9d48ce466703c79a0f37453cbe
suswanj <suswan.joglekar@gmail.com>
2025-02-16 01:10:49 +0530

    updated toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,9 @@
 return {
     "akinsho/toggleterm.nvim",
-    require("toggleterm").setup{},
+    version = "*",
+    opts = {
+        vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=20 direction=horizontal<CR>", { silent = true, noremap = true }),
+        vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm size=60 direction=vertical<CR>", { silent = true, noremap = true }),
+    }
 
-    vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=20 direction=horizontal<CR>", { silent = true, noremap = true }),
-    vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm size=60 direction=vertical<CR>", { silent = true, noremap = true }),
 }
