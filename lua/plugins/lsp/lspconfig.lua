7e95d9fe885df55445bd80f8d46fb099843f79e0
suswanj <suswan.joglekar@gmail.com>
2025-02-16 00:28:55 +0530

    added lspconfig plugin 'functionality setup complete'

---
+++ lua/plugins/lsp/lspconfig.lua
@@ -0,0 +1,97 @@
+return {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true },
+        { "folke/neodev.nvim", opts = {} },
+    },
+
+    config = function()
+        local lspconfig = require("lspconfig")
+        local mason_lspconfig = require("mason-lspconfig")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+        local map = vim.keymap.set
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function()
+                local opts = { buffer = ev.buf, silent = true }
+
+                map("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+                map("n", "gd", "<cmd>Telescope lsp_implementations<CR>", opts)
+                map("n", "gi", "<cmd>Telescope lsp_definitions<CR>", opts)
+                map("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+                map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                map("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+                map("n", "<leader>d", vim.diagnostic.open_float, opts)
+                map("n", "[d", vim.diagnostic.goto_prev, opts)
+                map("n", "]d", vim.diagnostic.goto_next, opts)
+                map("n", "<leader>rs", ":LspRestart<CR>", opts)
+            end,
+        })
+
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+
+        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
+        mason_lspconfig.setup_handlers({
+            -- default handler for installed servers
+            function(server_name)
+                lspconfig[server_name].setup({
+                capabilities = capabilities,
+                })
+            end,
+            ["svelte"] = function()
+                -- configure svelte server
+                lspconfig["svelte"].setup({
+                capabilities = capabilities,
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePost", {
+                    pattern = { "*.js", "*.ts" },
+                    callback = function(ctx)
+                        -- Here use ctx.match instead of ctx.file
+                        client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                    end,
+                    })
+                end,
+                })
+            end,
+            ["graphql"] = function()
+                -- configure graphql language server
+                lspconfig["graphql"].setup({
+                capabilities = capabilities,
+                filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+                })
+            end,
+            ["emmet_ls"] = function()
+                -- configure emmet language server
+                lspconfig["emmet_ls"].setup({
+                capabilities = capabilities,
+                filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+                })
+            end,
+            ["lua_ls"] = function()
+                -- configure lua server (with special settings)
+                lspconfig["lua_ls"].setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                    -- make the language server recognize "vim" global
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    completion = {
+                        callSnippet = "Replace",
+                    },
+                    },
+                },
+            })
+        end,
+        })
+    end,
+}
