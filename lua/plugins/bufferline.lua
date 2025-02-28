6ab3f5d844fc36d63d744ac832a4929c47770d05
suswanj <suswan.joglekar@gmail.com>
2025-03-01 00:02:46 +0530

    updated bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,10 +1,28 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    version = "*",
+    opts = {
+        options = {
+            show_close_icon = false,
+            diagnostics = 'nvim_lsp',
+            max_prefix_length = 8,
+            diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            if context.buffer:current() then
+                return ''
+            end
+            if level:match('error') then
+                return ' ' .. vim.g.diagnostic_icons.Error
+            elseif level:match('warning') then
+                return ' ' .. vim.g.diagnostic_icons.Warning
+            end
+            return ''
+            end,
+            custom_filter = function(buf_number, buf_numbers)
+            if vim.bo[buf_number].filetype ~= 'oil' then
+                return true
+            end
+            end,
+        },
     },
-  },
 }
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,5 +3,10 @@ return {
     config = function()
         vim.g.copilot_enabled = 0;
         vim.g.copilot_filetype = {};
+
+        --- Keymaps for copilot
+        vim.api.nvim_set_keymap('n', '<leader>cpe', ':Copilot enable<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>cpd', ':Copilot disable<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('i', '<C-g>', 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
     end
 }
