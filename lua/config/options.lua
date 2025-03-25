319c0f530378a3764082390c1ecbcef1e74adf5b
suswanj <suswan.joglekar@gmail.com>
2025-03-25 16:22:06 +0530

    options update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -82,6 +82,16 @@ opt.shiftwidth = 4
 opt.tabstop = 4
 opt.smartindent = true
 opt.shiftround = true
+opt.expandtab = true
+
+--- for webdev
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+    callback = function ()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end
+})
 
 -----------------------------------------------
 --- Formatting
